2f8279fb3c43581ab517403584905ace8265c25f
HadiNiazi <hadiniazi801@gmail.com>
2022-08-19 22:18:39 +0500

    Uploaded at the end of series

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\Auth\DashboardController;
+use App\Http\Controllers\Auth\PostController;
+use App\Http\Controllers\WebsiteController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('contact-us', 'website.contact')->name('contact');
+Route::controller(WebsiteController::class)->group(function() {
+    Route::get('/', 'home')->name('home');
+    Route::get('/posts/{post}', 'show')->name('website.posts.show');
+});
+
+Auth::routes();
+
+Route::prefix('auth')->middleware(['auth'])->group(function() {
+    Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('auth.dashboard');
+    Route::resource('posts', PostController::class);
+});
+
